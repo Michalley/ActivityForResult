1efd77c443bf480d91f379a979a252178d2c88ff
michalley <ml5877@bs.amalnet.k12.il>
2018-06-11 01:26:14 +0300

    1st commit

---
+++ app/src/main/java/com/example/activityforresult/SecondActivity.java
@@ -0,0 +1,37 @@
+package com.example.activityforresult;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import javax.xml.transform.Result;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView tv;
+    Button btnb;
+    Intent gi;
+    Intent back;
+    String ssumm;
+    double sum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        tv=findViewById(R.id.tv);
+        btnb=findViewById(R.id.btnb);
+        gi=getIntent();
+        ssumm=gi.getStringExtra("sum");
+        sum= Double.parseDouble(ssumm);
+        tv.setText(ssumm);
+    }
+    public void Back(View view) {
+        back.putExtra("start", sum);
+        setResult(RESULT_OK, back);
+        finish();
+    }
+}
